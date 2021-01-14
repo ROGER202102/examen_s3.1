28942678cbc1dd165d8600ba21d2e58e34b5f704
ROGER202102 <77450943+ROGER202102@users.noreply.github.com>
2021-01-14 12:26:27 -0500

    Add files via upload

---
+++ semana12/js/arrays4.js
@@ -0,0 +1,207 @@
+var vehiculos=new Array();
+var vehi1=new Object();
+vehi1.nombre="toyota";
+vehi1.modelo="hilux";
+vehi1.color="negro";
+vehi1.diseño="hilux";
+vehi1.motor="400 cabballos de fuerza";
+vehi1.llantas="peladas";
+vehi1.ventanas="polarisadas";
+
+var vehi2=new Object();
+vehi2.nombre="muscle car";
+vehi2.modelo="mustahg";
+vehi2.color="plateado";
+vehi2.diseño="mustang";
+vehi2.motor="estetico y vanguardista";
+vehi2.llantas="aerodinamicos";
+vehi2.ventanas="transparentes";
+
+var vehi3=new Object();
+vehi3.nombre="van";
+vehi3.modelo="furgoneta";
+vehi3.color="blanco";
+vehi3.diseño="van";
+vehi3.tipo="turistico";
+vehi3.llantas="aerodinamicos";
+vehi3.ventanas="transparentes";
+
+var vehi4=new Object();
+vehi4.nombre="suv";
+vehi4.modelo="todoterreno";
+vehi4.color="azul";
+vehi4.diseño="hilux";
+vehi4.motor="400 caballos de fuerza";
+vehi4.llantas="aerodinamicos";
+vehi4.ventanas="polarisadas";
+
+var vehi5=new Object();
+vehi5.nombre="alfa romero";
+vehi5.modelo="stelvio";
+vehi5.color="azul";
+vehi5.diseño="mito";
+vehi5.motor="400 caballos de fuerza";
+vehi5.llantas="aerodimanicas";
+vehi5.ventanas="transparentes";
+
+var vehi6=new Object();
+vehi6.nombre="bentley";
+vehi6.modelo="continetal GT";
+vehi6.color="blamco";
+vehi6.diseño="flyng Spur";
+vehi6.motor="400 caballos de fuerza";
+vehi6.llantas="aerodinamicas";
+vehi6.ventanas="polarisadas";
+
+var vehi7=new Object();
+vehi7.nombre="audi";
+vehi7.modelo="A7 sportbak";
+vehi7.color="verde";
+vehi7.diseño="q5";
+vehi7.motor="400 caballos de fuerza";
+vehi7.llantas="aerodinamicas";
+vehi7.ventanas="polarisadas";
+
+var vehi8=new Object();
+vehi8.nombre="albart";
+vehi8.modelo="albarth";
+vehi8.color="azul";
+vehi8.diseño="spider124";
+vehi8.motor="400 caballos de fuerza";
+vehi8.llantas="aerodinamicas";
+vehi8.ventanas="polarisadas";
+
+var vehi9=new Object();
+vehi9.nombre="bugatti";
+vehi9.modelo="veyron";
+vehi9.color="dorado";
+vehi9.diseño="chiron";
+vehi9.motor="400 caballos de fuerza";
+vehi9.llantas="aerodinamicas";
+vehi9.ventanas="polarisadas";
+
+var vehi10=new Object();
+vehi10.nombre="ferrari";
+vehi10.modelo="458 italia";
+vehi10.color="azul";
+vehi10.diseño="458 italia";
+vehi10.motor="400 caballos de fuerza";
+vehi10.llantas="aerodinamicas";
+vehi10.ventanas="polarisadas";
+
+var vehi11=new Object();
+vehi11.nombre="KIA";
+vehi11.modelo="stonic";
+vehi11.color="blanco";
+vehi11.diseño="cee'd";
+vehi11.motor="400 caballos de fuerza";
+vehi11.llantas="aerodinamicas";
+vehi11.ventanas="polarisadas";
+
+var vehi12=new Object();
+vehi12.nombre="jeep";
+vehi12.modelo="wrangler";
+vehi12.color="azul";
+vehi12.diseño="cherokee";
+vehi12.motor="400 caballos de fuerza";
+vehi12.llantas="aerodinamicas";
+vehi12.ventanas="polarisadas";
+
+var vehi13=new Object();
+vehi13.nombre="lamborghini";
+vehi13.modelo="aventador";
+vehi13.color="negro";
+vehi13.diseño="huracan";
+vehi13.motor="400 caballos de fuerza";
+vehi13.llantas="aerodinamicas";
+vehi13.ventanas="polarisadas";
+
+var vehi14=new Object();
+vehi14.nombre="lexus";
+vehi14.modelo="nx";
+vehi14.color="blando";
+vehi14.diseño="GS";
+vehi14.motor="400 caballos de fuerza";
+vehi14.llantas="aerodinamicas";
+vehi14.ventanas="polarisadas";
+
+var vehi15=new Object();
+vehi15.nombre="mercedes benz";
+vehi15.modelo="clase s";
+vehi15.color="azul";
+vehi15.diseño="hilux";
+vehi15.motor="400 caballos de fuerza";
+vehi15.llantas="aerodinamicas";
+vehi15.ventanas="polarisadas";
+
+var vehi16=new Object();
+vehi16.nombre="nissan";
+vehi16.modelo="leaf";
+vehi16.color="blanco";
+vehi16.diseño="pulsar";
+vehi16.motor="400 caballos de fuerza";
+vehi16.llantas="aerodinamicas";
+vehi16.ventanas="polarisadas";
+
+var vehi17=new Object();
+vehi17.nombre="ssang yong";
+vehi17.modelo="rexton";
+vehi17.color="morado";
+vehi17.diseño="tivoli";
+vehi17.motor="400 caballos de fuerza";
+vehi17.llantas="aerodinamicas";
+vehi17.ventanas="polarisadas";
+
+var vehi18=new Object();
+vehi18.nombre="tesla";
+vehi18.modelo="modelo x";
+vehi18.color="blanco";
+vehi18.diseño="roadster";
+vehi18.motor="400 caballos de fuerza";
+vehi181.llantas="aerodinamicas";
+vehi18.ventanas="polarisadas";
+
+var vehi19=new Object();
+vehi19.nombre="volkswagen";
+vehi19.modelo="polo";
+vehi19.color="celeste";
+vehi19.diseño="tiguan";
+vehi19.motor="400 caballos de fuerza";
+vehi19.llantas="aerodinamicas";
+vehi19.ventanas="polarisadas";
+
+var vehi20=new Object();
+vehi20.nombre="suxuki";
+vehi20.modelo="swift";
+vehi20.color="blanco";
+vehi20.diseño="vitara";
+vehi20.motor="400 caballos de fuerza";
+vehi20.llantas="aerodinamicas";
+vehi20.ventanas="polarisadas";
+
+
+
+vehiculos.push (vehi1);
+vehiculos.push (vehi2);
+vehiculos.push (vehi3);
+vehiculos.push (vehi4);
+vehiculos.push (vehi5);
+vehiculos.push (vehi6);
+vehiculos.push (vehi7);
+vehiculos.push (vehi8);
+vehiculos.push (vehi9);
+vehiculos.push (vehi10);
+vehiculos.push (vehi11);
+vehiculos.push (vehi12);
+vehiculos.push (vehi13);
+vehiculos.push (vehi14);
+vehiculos.push (vehi15);
+vehiculos.push (vehi16);
+vehiculos.push (vehi17);
+vehiculos.push (vehi18);
+vehiculos.push (vehi19);
+vehiculos.push (vehi20);
+
+function mostrar(){
+    alert(vehiculos [1].nombre);
+}
